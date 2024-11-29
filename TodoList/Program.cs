57c539057c3331007104873b0e1924539a235be5
David Maldonado <arathmaldonadomarquez@gmail.com>
2024-11-28 21:09:34 -0500

    16.variables 5:36

--- TodoList/Program.cs
+++ TodoList/Program.cs
@@ -3,4 +3,13 @@ Console.WriteLine("[S]ee all TODOs");
 Console.WriteLine("[A]dd a TODO");
 Console.WriteLine("[R]emove a TODO");
 Console.WriteLine("[E]xit");
+
+string userInput = "A";
+Console.WriteLine(userInput);
+userInput = "ABC";
+Console.WriteLine(userInput);
+
+int number = 2;
+number = 7;
+
 Console.ReadKey();
