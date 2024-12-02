cf6991215ca256e9ffc44e6e5dabe19b4f1a206a
David Maldonado <arathmaldonadomarquez@gmail.com>
2024-12-02 08:05:47 -0500

    19. Implicitly typed variables 0.00

--- TodoList/Program.cs
+++ TodoList/Program.cs
@@ -1,4 +1,15 @@
 ﻿int a = 10;
 int b = 5;
+++a;
+--b;
+Console.WriteLine(a);
+Console.WriteLine(b);
+
+Console.WriteLine("Addition: " + (a + b));
+Console.WriteLine("Substraction: " + (a - b));
+Console.WriteLine("Multiplication: " + a * b);
+Console.WriteLine("Division: " + a / b);
+Console.WriteLine("Jonh" + " " + "Smith");
+
 
 Console.ReadKey();
