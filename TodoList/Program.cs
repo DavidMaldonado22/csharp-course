6c1022e57e69dc5ba9ed889a690bad4ab3e61258
David Maldonado <arathmaldonadomarquez@gmail.com>
2025-02-02 12:42:39 -0500

    24. Boolean type : 6:00

--- TodoList/Program.cs
+++ TodoList/Program.cs
@@ -6,9 +6,19 @@ Console.WriteLine("[R]emove a TODO");
 Console.WriteLine("[E]xit");
 
 string UserChoice = Console.ReadLine();
-Console.WriteLine("User input: " + UserChoice);
-UserChoice = "ABC";
 
-var result = "abc" + "def" + "ghi";
+bool isUserInputAbc = UserChoice == "ABC";
+bool isUserInputNotAbc = UserChoice != "ABC";
+bool isUserInputNotAbc2 = !(UserChoice == "ABC");
+
+var number = 10;
+
+var isLargerThan5 = number > 5;
+var isSmallerThan10 = number < 10;
+var isLargerOrEqualTo10 = number >= 10;
+var isSmallerOrEqualTo6 = number <= 6;
+
+var is10Modulo3EqualTo1 = 10 % 3 == 1;
+var isEven = 10 % 2 == 0;
 
 Console.ReadKey(); 
