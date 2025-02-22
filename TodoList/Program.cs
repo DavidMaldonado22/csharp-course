93019a2fab8c5ca1e1d56e1fb4a0b3d8ffdd23f9
David Maldonado <arathmaldonadomarquez@gmail.com>
2025-02-22 16:03:51 -0500

    27. The scope of local varaibles:5:00

--- TodoList/Program.cs
+++ TodoList/Program.cs
@@ -1,4 +1,6 @@
-﻿Console.WriteLine("Hello");
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+Console.WriteLine("Hello");
 Console.WriteLine("What do you want to do?");
 Console.WriteLine("[S]ee all TODOs");
 Console.WriteLine("[A]dd a TODO");
@@ -7,18 +9,28 @@ Console.WriteLine("[E]xit");
 
 var userChoice = Console.ReadLine();
 
-if(userChoice.Length <= 3)
+if (userChoice.Length == 0)
 {
-    Console.WriteLine("Short answer");
-}
-else if(userChoice.Length < 10)
-{
-    Console.WriteLine("Medium answer");
+    Console.WriteLine("Empty choice!");
+    int number = 5;
+    Console.WriteLine(number);
+    var word = "ABC";
+    var number2 = 10;
+    if(word.Length > 0)
+    {
+        var number2 = 10;
+        Console.WriteLine(number);
+        Console.WriteLine(userChoice);
+    }
+    Console.WriteLine(number2);
 }
 else
 {
-    Console.WriteLine("Long answer");
+    int number = 5;
+    Console.WriteLine("Non-empty choice: " + userChoice);
+    Console.WriteLine(number);
 }
-
+Console.WriteLine("Your choice is: " + userChoice);
+Console.WriteLine(number);
 
 Console.ReadKey(); 
