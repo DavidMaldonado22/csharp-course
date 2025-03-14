4fb5bb59669050c3c8a335489e5402fda9839485
David Maldonado <arathmaldonadomarquez@gmail.com>
2025-03-14 17:30:32 -0500

    35. The first exception: 2:00

--- TodoList/Program.cs
+++ TodoList/Program.cs
@@ -1,17 +1,22 @@
 ﻿using System.Runtime.CompilerServices;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
-Console.WriteLine("Hello");
-Console.WriteLine("What do you want to do?");
-Console.WriteLine("[S]ee all TODOs");
-Console.WriteLine("[A]dd a TODO");
-Console.WriteLine("[R]emove a TODO");
-Console.WriteLine("[E]xit");
+Console.WriteLine("Provide a number.");
+string userInput = Console.ReadLine();
+int number = int.Parse(userInput);
+Console.WriteLine(number);
+
+//Console.WriteLine("Hello");
+//Console.WriteLine("What do you want to do?");
+//Console.WriteLine("[S]ee all TODOs");
+//Console.WriteLine("[A]dd a TODO");
+//Console.WriteLine("[R]emove a TODO");
+//Console.WriteLine("[E]xit");
 
 var userChoice = Console.ReadLine();
 
 var word = "abc";
-word = 10;
+word = "abc";
 
 bool isLong = IsLong(word);
 
