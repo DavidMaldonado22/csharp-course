056afcc17ab20e61c6e659d8c91a0dbf51fb7587
David Maldonado <arathmaldonadomarquez@gmail.com>
2025-04-23 20:27:06 -0500

    59. "out" keyword

--- TodoList/Program.cs
+++ TodoList/Program.cs
@@ -1,12 +1,36 @@
 ﻿using System.Runtime.CompilerServices;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
-List<string> words = new List<string>();
+int[] numbers = new[] {10, -8, 2, 12, -17};
 
-Console.WriteLine("Count of elements is " + words.Count);
+int nonPositiveCount;
 
-words.Add("hello");
+List<int> onlyPositive = GetOnlyPositive(numbers, out nonPositiveCount);
 
-Console.WriteLine("Count of elements is " + words.Count);
+foreach (int number in onlyPositive)
+{
+    Console.WriteLine(number);
+}
+
+Console.WriteLine("Count of non positive " + nonPositiveCount);
+
+List<int> GetOnlyPositive(int[] numbers, out int countOfNonPositive)
+{
+    List<int> result = new List<int>();
+    countOfNonPositive = 0;
+
+    foreach(int number in numbers)
+    {
+
+        if (number > 0) {
+            result.Add(number);
+        }
+        else
+        {
+            countOfNonPositive = countOfNonPositive + 1;
+        }
+    }
+    return result;
+}
 
 Console.ReadKey();
